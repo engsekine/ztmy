24cf774d230a07a34da2162f36cf39a3721045d7
engsekine <engsekine@gmail.com>
2022-06-14 23:10:24 +0900

    update

--- memo/gulpfile.js
+++ memo/gulpfile.js
@@ -1,26 +1,24 @@
-const gulp = require('gulp');//gulp本体
+const gulp = require("gulp"); //gulp本体
 
 //scss
-const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
+const sass = require("gulp-dart-sass"); //Dart Sass はSass公式が推奨 @use構文などが使える
 const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
 const notify = require("gulp-notify"); // エラー発生時のアラート出力
 const browserSync = require("browser-sync"); //ブラウザリロード
 
-
 // 入出力するフォルダを指定
-const srcBase = '../_static/src';
-const assetsBase = '../_assets';
-const distBase = '../_static/dist';
-
+const srcBase = "_static/src";
+const assetsBase = "_assets";
+const distBase = "_static/dist";
 
 const srcPath = {
-  'scss': assetsBase + '/scss/**/*.scss',
-  'html': srcBase + '/**/*.html'
+  scss: assetsBase + "/scss/**/*.scss",
+  html: srcBase + "/**/*.html",
 };
 
 const distPath = {
-  'css': distBase + '/css/',
-  'html': distBase + '/'
+  css: distBase + "/css/",
+  html: distBase + "/",
 };
 
 /**
@@ -28,42 +26,44 @@ const distPath = {
  *
  */
 const cssSass = () => {
-  return gulp.src(srcPath.scss, {
-    sourcemaps: true
-  })
+  return gulp
+    .src(srcPath.scss, {
+      sourcemaps: true,
+    })
     .pipe(
       //エラーが出ても処理を止めない
       plumber({
-        errorHandler: notify.onError('Error:<%= error.message %>')
-      }))
-    .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
-    .pipe(gulp.dest(distPath.css, { sourcemaps: './' })) //コンパイル先
+        errorHandler: notify.onError("Error:<%= error.message %>"),
+      })
+    )
+    .pipe(sass({ outputStyle: "expanded" })) //指定できるキー expanded compressed
+    .pipe(gulp.dest(distPath.css, { sourcemaps: "./" })) //コンパイル先
     .pipe(browserSync.stream())
-    .pipe(notify({
-      message: 'Sassをコンパイルしました！',
-      onLast: true
-    }))
-}
-
+    .pipe(
+      notify({
+        message: "Sassをコンパイルしました！",
+        onLast: true,
+      })
+    );
+};
 
 /**
  * html
  */
 const html = () => {
-  return gulp.src(srcPath.html)
-    .pipe(gulp.dest(distPath.html))
-}
+  return gulp.src(srcPath.html).pipe(gulp.dest(distPath.html));
+};
 
 /**
  * ローカルサーバー立ち上げ
  */
 const browserSyncFunc = () => {
   browserSync.init(browserSyncOption);
-}
+};
 
 const browserSyncOption = {
-  server: distBase
-}
+  server: distBase,
+};
 
 /**
  * リロード
@@ -71,7 +71,7 @@ const browserSyncOption = {
 const browserSyncReload = (done) => {
   browserSync.reload();
   done();
-}
+};
 
 /**
  *
@@ -80,9 +80,9 @@ const browserSyncReload = (done) => {
  * watch('監視するファイル',処理)
  */
 const watchFiles = () => {
-  gulp.watch(srcPath.scss, gulp.series(cssSass))
-  gulp.watch(srcPath.html, gulp.series(html, browserSyncReload))
-}
+  gulp.watch(srcPath.scss, gulp.series(cssSass));
+  gulp.watch(srcPath.html, gulp.series(html, browserSyncReload));
+};
 
 /**
  * seriesは「順番」に実行
